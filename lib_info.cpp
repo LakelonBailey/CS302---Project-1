581d9a1386dd74e0476fec159b825fe2571b4078
OutliersAdvantage <102266508+LakelonBailey@users.noreply.github.com>
2023-01-27 10:01:09 -0500

    completed all data storing

--- lib_info.cpp
+++ lib_info.cpp
@@ -5,6 +5,12 @@
 
 using namespace std;
 
+/***********************************
+********* HELPER FUNCTIONS *********
+************************************/
+
+
+// Return a vector of strings split on the provided separator
 vector<string> split(string line, char on) {
     vector<string> strings;
     string item = "";
@@ -20,6 +26,7 @@ vector<string> split(string line, char on) {
     return strings;
 }
 
+// Join a vector of strings on the provided separator
 string join(vector<string> strings, char separator) {
     string result = "";
     for (size_t i = 0; i < strings.size(); i++) {
@@ -29,10 +36,12 @@ string join(vector<string> strings, char separator) {
     return result;
 }
 
+// Replace every instance of a char in a string with a different char
 string replace(string item, char toReplace, char replaceWith) {
     return join(split(item, toReplace), replaceWith);
 }
 
+// Convert numeric string to integer
 int intString(string num) {
     istringstream ss;
     int val;
@@ -41,6 +50,14 @@ int intString(string num) {
     return val;
 }
 
+// Convert integer to string
+string stringInt(int num) {
+    ostringstream ss;
+    ss << num;
+    return ss.str();
+}
+
+// Convert time in format MM:SS to total seconds as an integer
 int calculateTime(string timeString) {
     vector <string> timeStrings = split(timeString, ':');
     int time = 0;
@@ -48,6 +65,19 @@ int calculateTime(string timeString) {
     time += intString(timeStrings[1]);
     return time;
 }
+
+// Convert time in total seconds to MM:SS format
+string formatTime(int time) {
+    int minutes = time / 60;
+    int seconds = time % 60;
+    return stringInt(minutes) + ":" + stringInt(seconds);
+}
+
+
+/***********************************
+************ STRUCTURES ************
+************************************/
+
 
 struct Song {
     string title;
@@ -59,12 +89,19 @@ struct Song {
         this->track = intString(trackString);
         this->time = calculateTime(timeString);
     }
+
+    void print() {
+        printf("                %d. %s: %s\n", track, title.c_str(), formatTime(time).c_str());
+    }
 };
 
 struct Album {
     vector<Song> songs;
     string name;
-    int nsongs = 1;  // optional variable but makes it easier
+
+    Album(string name) {
+        this->name = name;
+    }
 
     int time() {
         int total = 0;
@@ -73,23 +110,70 @@ struct Album {
         }
         return total;
     }
+
+    int songCount() {
+        return (int)songs.size();
+    }
+
+    void print() {
+        printf("        %s, %d, %s\n", name.c_str(), songCount(), formatTime(time()).c_str());
+        for (size_t i = 0; i < songs.size(); i++) {
+            songs[i].print();
+        }
+    }
 };
 
 struct Artist {
-    vector<Album> albums;
+    vector<Album *> albums;
     string name;
-    int time = 0;
-    int nsongs = 0;
 
     Artist(string name) {
         this->name = name;
     }
 
-    void addAlbum(Album album) {
-        this->albums.push_back(album);
-        this->time += album.time();
+    ~Artist() {
+        for (size_t i = 0; i < albums.size(); i++) {
+            delete albums[i];
+        }
+    }
+
+    Album * getAlbum(string albumName) {
+        for (size_t i = 0; i < albums.size(); i++) {
+            if (albums[i]->name == albumName) {
+                return albums[i];
+            }
+        }
+        Album * album = new Album(albumName);
+        albums.push_back(album);
+        return album;
+    }
+
+    int songCount() {
+        int sum = 0;
+        for (size_t i = 0; i < albums.size(); i++) sum += albums[i]->songCount();
+        return sum;
+    }
+
+    string getTime() {
+        int timeSum = 0;
+        for (size_t i = 0; i < albums.size(); i++) {
+            timeSum += albums[i]->time();
+        }
+        return formatTime(timeSum);
+    }
+
+    void print() {
+        printf("%s: %d, %s\n", name.c_str(), songCount(), getTime().c_str());
+        for (size_t i = 0; i < albums.size(); i++) {
+            albums[i]->print();
+        }
     }
 };
+
+
+/***********************************
+*************** MAIN ***************
+************************************/
 
 int main(int argc, char *argv[]) {
     ifstream fin;
@@ -98,11 +182,11 @@ int main(int argc, char *argv[]) {
     vector<Artist *> artists;
 
     while (getline(fin, line)) {
-        vector <string> items = split(line, ' ');
+        vector <string> songInfo = split(line, ' ');
         Artist * artist = nullptr;
         bool newArtist = true;
 
-        string artistName = replace(items[2], '_', ' ');
+        string artistName = replace(songInfo[2], '_', ' ');
         for (size_t i = 0; i < artists.size(); i++) {
             if (artists[i]->name == artistName) {
                 artist = artists[i];
@@ -116,10 +200,19 @@ int main(int argc, char *argv[]) {
             artists.push_back(artist);
         }
 
+        string albumName = replace(songInfo[3], '_', ' ');
+        Album * album = artist->getAlbum(albumName);
+
+        string songTitle = replace(songInfo[0], '_', ' ');
+        string timeString = songInfo[1];
+        string trackString = songInfo[5];
+        Song song = Song(songTitle, timeString, trackString);
+        album->songs.push_back(song);
+
     }
 
     for (size_t i = 0; i < artists.size(); i++) {
-        cout<<artists[i]->name<<endl;
+        artists[i]->print();
         delete artists[i];
     }
 
